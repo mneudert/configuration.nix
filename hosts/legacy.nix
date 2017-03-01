0801c157f53aafac1b26621b6124ce9651ad57de
Marc Neudert <marc.neudert@gmail.com>
2017-03-01 20:47:17 +0100

    extracts system defaults

--- hosts/legacy.nix
+++ hosts/legacy.nix
@@ -6,6 +6,8 @@
       ./legacy/hardware-configuration.nix
       ./legacy/system-packages.nix
       ./legacy/custom-packages.nix
+
+      ../system/default.nix
     ];
 
   boot.loader.grub.enable  = true;
@@ -14,24 +16,11 @@
 
   networking.hostName = "legacy";
 
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "de";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  time.timeZone = "Europe/Berlin";
-
-  services.openssh.enable = true;
-
-  users.defaultUserShell = "/run/current-system/sw/bin/zsh";
-  users.extraUsers.mne   = {
+  users.extraUsers.mne = {
     extraGroups = [ "wheel" ];
     isNormalUser = true;
     uid = 1000;
   };
 
   system.stateVersion = "16.09";
-
-  security.sudo.enable = true;
 }
---
+++ system/default.nix
@@ -0,0 +1,15 @@
+{
+  i18n = {
+    consoleFont   = "Lat2-Terminus16";
+    consoleKeyMap = "de";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  time.timeZone = "Europe/Berlin";
+
+  security.sudo.enable = true;
+
+  services.openssh.enable = true;
+
+  users.defaultUserShell = "/run/current-system/sw/bin/zsh";
+}
