03e993b7dda1e68ae503464e77a6712210e3da84
Marc Neudert <marc.neudert@gmail.com>
2017-03-01 21:04:14 +0100

    defines default "~/.profile"

--- users/mne.nix
+++ users/mne.nix
@@ -1,7 +1,21 @@
+{ lib, ... }:
+
+with lib;
 {
   users.extraUsers.mne = {
     extraGroups  = [ "wheel" ];
     isNormalUser = true;
     uid          = 1000;
+  };
+
+  system.activationScripts = {
+    dotfiles_mne = stringAfter [ "users" ]
+    ''
+      cd /home/mne
+
+      ln -fs ${../dotfiles/profile} .profile
+
+      chown -h mne:users .profile
+    '';
   };
 }
