293091e920b165fdd12cc806133865d2ec22fd7d
Marc Neudert <marc.neudert@gmail.com>
2017-03-23 19:42:42 +0100

    [packages] init kibana_52 at v5.2.2

---
+++ packages/kibana_52/default.nix
@@ -0,0 +1,24 @@
+{ stdenv, fetchurl, makeWrapper, nodejs, jre, utillinux }:
+
+stdenv.mkDerivation rec {
+  name    = "kibana-${version}";
+  version = "5.2.2";
+
+  src = fetchurl {
+    url = "https://artifacts.elastic.co/downloads/kibana/kibana-${version}-linux-x86_64.tar.gz";
+    sha256 = "0qrmlcv0anjywmalqdxz2vzq5h9ffnfrzyyrmripznl6w9n54z4w";
+  };
+
+  buildInputs = [ makeWrapper jre utillinux nodejs ];
+
+  installPhase = ''
+    mkdir -p $out/libexec/kibana $out/bin
+    mv * $out/libexec/kibana/
+    rm -r $out/libexec/kibana/node
+
+    makeWrapper $out/libexec/kibana/bin/kibana $out/bin/kibana \
+      --prefix PATH : "${stdenv.lib.makeBinPath [ nodejs ]}"
+
+    sed -i 's@NODE=.*@NODE=${nodejs}/bin/node@' $out/libexec/kibana/bin/kibana
+  '';
+}
