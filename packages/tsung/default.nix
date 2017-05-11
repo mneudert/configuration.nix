57c5f73cea99d382cd0c4eb33cec69aacba6d900
Marc Neudert <marc.neudert@gmail.com>
2017-05-11 18:49:59 +0200

    [packages] init tsung at 1.6.0

---
+++ packages/tsung/default.nix
@@ -0,0 +1,15 @@
+{ stdenv, fetchurl, pkgs }:
+
+stdenv.mkDerivation rec {
+  name    = "tsung";
+  version = "1.6.0";
+
+  src = fetchurl {
+    url    = "http://tsung.erlang-projects.org/dist/tsung-${version}.tar.gz";
+    sha256 = "111xxchbdc5x3vpycbqy952868sj627wnc33lzckfw7xj0x6r12n";
+  };
+
+  buildInputs = with pkgs; [
+    erlang
+  ];
+}
