1a00a0ca10127245a2bf264e84f1f1d9ff479a5e
Marc Neudert <marc.neudert@gmail.com>
2017-05-26 14:35:13 +0200

    [packages] init kibana6 at 6.0.0-alpha1

---
+++ packages/kibana6/default.nix
@@ -0,0 +1,24 @@
+{ stdenv, fetchurl, makeWrapper, nodejs, jre, utillinux }:
+
+stdenv.mkDerivation rec {
+  name    = "kibana-${version}";
+  version = "6.0.0-alpha1";
+
+  src = fetchurl {
+    url = "https://artifacts.elastic.co/downloads/kibana/kibana-${version}-linux-x86_64.tar.gz";
+    sha256 = "04zsib5hlnv5lba5yydqnsqirc94d6ydjfi6xb243jllgwb3farc";
+  };
+
+  buildInputs = [ makeWrapper jre utillinux nodejs ];
+
+  installPhase = ''
+    mkdir -p $out/libexec/kibana $out/bin
+    mv * $out/libexec/kibana/
+    rm -r $out/libexec/kibana/node
+
+    makeWrapper $out/libexec/kibana/bin/kibana $out/bin/kibana \
+      --prefix PATH : "${stdenv.lib.makeBinPath [ nodejs ]}"
+
+    sed -i 's@NODE=.*@NODE=${nodejs}/bin/node@' $out/libexec/kibana/bin/kibana
+  '';
+}
