c78aa84af02d487cca5cf351a81ab584b14d5752
Marc Neudert <marc.neudert@gmail.com>
2017-07-07 17:33:29 +0200

    elasticsearch5: 5.4.2 -> 5.5.0

--- packages/elasticsearch5/default.nix
+++ packages/elasticsearch5/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name    = "elasticsearch-${version}";
-  version = "5.4.3";
+  version = "5.5.0";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${version}.tar.gz";
-    sha256 = "13y66662nhh9rn49d2fn4r1s82dbrj8jkrnw34snz6iy4jigdshc";
+    sha256 = "0bizfpnw9mjspshwlqgkx3jb95374bj423q9i68yznmi8s56ncda";
   };
 
   patches = [
