6623f89ecf8a76e160859ff55775e3ba8ef0ad0a
Marc Neudert <marc.neudert@gmail.com>
2017-07-17 18:39:09 +0200

    [packages] bash-todo: 435c77c -> 40e3631

--- packages/bash-todo/default.nix
+++ packages/bash-todo/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name    = "bash-todo";
-  version = "435c77c";
+  version = "40e3631";
 
   src = fetchFromGitHub {
     owner  = "mneudert";
     repo   = "${name}";
-    rev    = "435c77c928b94bab36c5e4f51b9314579d7f3a20";
-    sha256 = "08alk3wnv03md8pfxiwdzz40pdz5dfmaklxbdnq67h4hmfixa2rm";
+    rev    = "40e36312dd3b91ee29c8c2594fb131f8c92723bc";
+    sha256 = "01j0bwx7jlwbf509fz6dvi7gkzdvivicqpg2h8pz0761cprmb0vq";
   };
 
   patchPhase   = "patchShebangs ./todo";
