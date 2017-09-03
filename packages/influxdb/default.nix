724f6a722083533bf8c0ce1f2c8f6b9ef25adae2
Marc Neudert <marc.neudert@gmail.com>
2017-09-03 14:30:03 +0200

    [packages] influxdb: 1.3.3 -> 1.3.5

--- packages/influxdb/default.nix
+++ packages/influxdb/default.nix
@@ -2,13 +2,13 @@
 
 buildGoPackage rec {
   name    = "influxdb-${version}";
-  version = "1.3.3";
+  version = "1.3.5";
 
   src = fetchFromGitHub {
     owner  = "influxdata";
     repo   = "influxdb";
     rev    = "v${version}";
-    sha256 = "146dagbdnwj9spqjmf0lzlir0x7ks9p07rr7c37rfdnf0ar7j6b3";
+    sha256 = "0cfxg813hidx63pyikflxb8d2zskyhz38ym9ms52bifw8zmrgvsl";
   };
 
   goDeps        =  ./deps.nix;
