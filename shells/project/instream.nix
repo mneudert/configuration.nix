b4945c293a4ceefbbe65b06c2196a5c2c7505316
Marc Neudert <marc.neudert@gmail.com>
2017-11-17 17:05:26 +0100

    [shells] auto-creates influxdb configuration for instream

--- shells/project/instream.nix
+++ shells/project/instream.nix
@@ -9,6 +9,8 @@ stdenv.mkDerivation rec {
 
   shellHook = ''
     PROJECT_ROOT="/data/projects/private/configuration.nix/runtime/instream"
+      
+    API_INFLUXDB='http://localhost:8086'
     CONF_INFLUXDB="$PROJECT_ROOT/influxdb.conf"
     LOG_INFLUXDB="$PROJECT_ROOT/influxdb.log"
     PID_INFLUXDB="$PROJECT_ROOT/influxdb.pid"
@@ -20,36 +22,50 @@ stdenv.mkDerivation rec {
       }
     }
 
-    function setup_influxdb {
-      local api_base='http://localhost:8086'
+    function restart_influxdb {
+      [ -f "$PID_INFLUXDB" ] && {
+        kill -TERM "$(cat "$PID_INFLUXDB")"
+        rm -f "$PID_INFLUXDB"
+      }
 
-      until curl -s -o /dev/null "$api_base"; do
+      influxd \
+          -config "$CONF_INFLUXDB" \
+        -pidfile "$PID_INFLUXDB" \
+      2>$LOG_INFLUXDB &
+
+      until curl -s -o /dev/null "$API_INFLUXDB"; do
         sleep 1
       done
+    }
 
-      curl "$api_base/query" \
+    function setup_influxdb {
+      curl "$API_INFLUXDB/query" \
           -X POST \
           --data-urlencode "q=CREATE USER instream_test WITH PASSWORD 'instream_test' WITH ALL PRIVILEGES" \
           -s -o /dev/null
 
-      curl "$api_base/query" \
+      curl "$API_INFLUXDB/query" \
           -X POST \
           -u instream_test:instream_test \
           --data-urlencode "q=CREATE USER instream_guest WITH PASSWORD 'instream_guest'" \
           -s -o /dev/null
+
+      sed -i 's/auth-enabled = false/auth-enabled = true/' "$CONF_INFLUXDB"
     }
 
     [ ! -d "$PROJECT_ROOT" ] && {
       mkdir -p $PROJECT_ROOT
       chmod 774 $PROJECT_ROOT
+
+      influxd config > "$CONF_INFLUXDB"
+
+      sed -i "s|/var/lib/influxdb|$PROJECT_ROOT|g" "$CONF_INFLUXDB"
+      echo -e "[[udp]]\n  enabled = true\n  bind-address = \":8089\"\n  database = \"test_database\"\n  batch-size = 1000\n  batch-timeout = \"1s\"\n  batch-pending = 5\n" >> "$CONF_INFLUXDB"
     }
 
-    influxd \
-        -config "$CONF_INFLUXDB" \
-        -pidfile "$PID_INFLUXDB" \
-    2>$LOG_INFLUXDB &
-
+    restart_influxdb
     setup_influxdb
+    restart_influxdb
 
     trap finish EXIT
 
