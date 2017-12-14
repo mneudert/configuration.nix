ce73f215fc021663d75633c9b76e0e01e8401375
Marc Neudert <marc.neudert@gmail.com>
2017-12-14 19:22:02 +0100

    [packages] gitcheck: 2017-12-05-fork -> 2017-12-14-fork

--- packages/gitcheck/default.nix
+++ packages/gitcheck/default.nix
@@ -2,13 +2,13 @@
 
 pkgs.python27Packages.buildPythonPackage rec {
   name    = "gitcheck-${version}";
-  version = "2017-12-05-fork";
+  version = "2017-12-14-fork";
 
   src = fetchFromGitHub {
     owner  = "mneudert";
     repo   = "gitcheck";
-    rev    = "fbfef71ddadb09c358bddbe28287b7fd27528cb0";
-    sha256 = "0ny0nham7zx76ark2451dvyhivj7v7n3qaypyzdrq75wjp884ljn";
+    rev    = "02f42fc8df3d1db3f0a8e26c84556221dde32ca3";
+    sha256 = "0xy9grz3xwj9lzivqvwkniqq00bzz6c0h4rqgpyssj139yvw09dn";
   };
 
   doCheck = false;
