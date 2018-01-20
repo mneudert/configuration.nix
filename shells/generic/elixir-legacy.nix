b3c065d2dcf58f31e16659471fba4bebdf4949d4
Marc Neudert <marc.neudert@gmail.com>
2018-01-20 13:13:20 +0100

    [shells] generic: elixir-legacy (1.3.4)

--- shells/generic/elixir-legacy.nix
+++ shells/generic/elixir-legacy.nix
@@ -1,18 +1,26 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
-  name = "generic-shell-elixir-legacy";
+  name = "generic-shell-elixir";
   env  = buildEnv {
     name  = name;
     paths = buildInputs;
   };
 
   shellHook = ''
+    export ERL_AFLAGS="-kernel shell_history enabled"
     export PS1="[${name}:\w]$ "
   '';
 
+  rebar = pkgs.rebar.override { erlang = erlangR20; };
+
+  elixir-legacy = pkgs.callPackage /data/projects/private/configuration.nix/packages/elixir-legacy {
+    erlang = erlangR20;
+    rebar  = rebar;
+  };
+
   buildInputs = [
-    beam.packages.erlangR18.elixir_1_3
-    erlangR18
+    elixir-legacy
+    erlangR20
   ];
 }
