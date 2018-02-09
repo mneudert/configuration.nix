4e430ac5c0d4c7c63dc61aaa086c41b8dd989967
Marc Neudert <marc.neudert@gmail.com>
2018-02-09 09:15:30 +0100

    [packages] kibana6: 6.1.3 -> 6.2.0

--- packages/kibana6/default.nix
+++ packages/kibana6/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name    = "kibana-${version}";
-  version = "6.1.3";
+  version = "6.2.0";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/kibana/kibana-${version}-linux-x86_64.tar.gz";
-    sha256 = "19z2i0xvrzafdyppa8dw2366bq3wvshfnk8ryn5qrjdnkp1g1jhr";
+    sha256 = "0dswi9alldsx116cy5prm9w1vr0m116di8z39s9y6wqy84rn02sw";
   };
 
   buildInputs = [ makeWrapper jre utillinux nodejs ];
