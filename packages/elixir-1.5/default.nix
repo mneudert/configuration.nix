3c33d3a4506de41125bb89c206ec0691af8d5e7f
Marc Neudert <marc.neudert@gmail.com>
2018-08-12 13:44:55 +0200

    [packages] elixir-1.5: init at 1.5.3

---
+++ packages/elixir-1.5/default.nix
@@ -0,0 +1,50 @@
+{ pkgs, stdenv, fetchFromGitHub, erlang, rebar, makeWrapper, coreutils, curl, bash }:
+
+stdenv.mkDerivation rec {
+  name = "elixir-${version}";
+  version = "1.5.3";
+
+  src = fetchFromGitHub {
+    owner = "elixir-lang";
+    repo = "elixir";
+    rev = "v${version}";
+    sha256 = "00kgqcn9g6vflc551wniz9pwv7pszyf8v6smpkqs50j3kbliihy5";
+  };
+
+  buildInputs = [ erlang rebar makeWrapper ];
+
+  LOCALE_ARCHIVE = stdenv.lib.optionalString stdenv.isLinux
+    "${pkgs.glibcLocales}/lib/locale/locale-archive";
+
+  LANG = "en_US.UTF-8";
+  LC_TYPE = "en_US.UTF-8";
+
+  setupHook = ./setup-hook.sh;
+
+  buildFlags = "ERL_COMPILER_OPTIONS=debug_info";
+
+  preBuild = ''
+    # The build process uses ./rebar. Link it to the nixpkgs rebar
+    rm -v rebar
+    ln -s ${rebar}/bin/rebar rebar
+
+    substituteInPlace Makefile \
+      --replace "/usr/local" $out
+  '';
+
+  postFixup = ''
+    # Elixir binaries are shell scripts which run erl. Add some stuff
+    # to PATH so the scripts can run without problems.
+
+    for f in $out/bin/*; do
+     b=$(basename $f)
+      if [ $b == "mix" ]; then continue; fi
+      wrapProgram $f \
+        --prefix PATH ":" "${stdenv.lib.makeBinPath [ erlang coreutils curl bash ]}" \
+        --set CURL_CA_BUNDLE /etc/ssl/certs/ca-certificates.crt
+    done
+
+    substituteInPlace $out/bin/mix \
+          --replace "/usr/bin/env elixir" "${coreutils}/bin/env elixir"
+  '';
+}
---
+++ shells/generic/elixir-1.5.nix
@@ -0,0 +1,26 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  name = "generic-shell-elixir-1.5";
+  env  = buildEnv {
+    name  = name;
+    paths = buildInputs;
+  };
+
+  shellHook = ''
+    export ERL_AFLAGS="-kernel shell_history enabled"
+    export PS1="[${name}:\w]$ "
+  '';
+
+  rebar = pkgs.rebar.override { erlang = erlangR20; };
+
+  elixir = pkgs.callPackage /data/projects/private/configuration.nix/packages/elixir-1.5 {
+    erlang = erlangR20;
+    rebar  = rebar;
+  };
+
+  buildInputs = [
+    elixir
+    erlangR20
+  ];
+}
