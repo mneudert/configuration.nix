3be08b87c793418691c9281947c2115444f6b80c
Marc Neudert <marc.neudert@gmail.com>
2018-09-08 13:03:22 +0200

    [shells] project: caylir_showcase

---
+++ shells/project/caylir_showcase.nix
@@ -0,0 +1,55 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  name = "project-shell-caylir_showcase";
+  env  = buildEnv {
+    name  = name;
+    paths = buildInputs;
+  };
+
+  shellHook = ''
+    PROJECT_ROOT="/data/projects/private/configuration.nix/runtime/caylir_showcase"
+    SHELL_LOCK="$PROJECT_ROOT/shell.lock"
+    SHELL_NAME="${name}"
+
+    function finish {
+      ps | grep 'cayley' | grep -v 'grep' | awk '{ print $1 }' | xargs kill
+
+      rm -f "$SHELL_LOCK"
+    }
+
+    if [ ! -f "$SHELL_LOCK" ]; then
+      mkdir -p "$(dirname "$SHELL_LOCK")"
+      touch "$SHELL_LOCK"
+
+      SHELL_NAME="$SHELL_NAME|\[\e[1m\]master\[\e[0m\]"
+
+      cayley http \
+        --db=memstore --dbpath="" \
+        --assets="${cayley}/assets" \
+      2>/dev/null &
+
+      trap finish EXIT
+    fi
+
+    export ERL_AFLAGS="-kernel shell_history enabled"
+    export PS1="[$SHELL_NAME:\w]$ "
+  '';
+
+  cayley = pkgs.callPackage /data/projects/private/configuration.nix/packages/cayley {};
+
+  rebar  = pkgs.rebar.override { erlang = erlangR20; };
+  elixir = pkgs.callPackage /data/projects/private/configuration.nix/packages/elixir-1.5 {
+    erlang = erlangR20;
+    rebar  = rebar;
+  };
+
+  buildInputs = [
+    cayley
+    elixir
+    erlangR20
+    nodejs-6_x
+
+    inotify-tools
+  ];
+}
