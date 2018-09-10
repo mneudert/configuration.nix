446c15cac0b273c18bad90212f3e2b98c68df4f2
Marc Neudert <marc.neudert@gmail.com>
2018-09-10 19:52:18 +0200

    [shells] template: cayley

---
+++ shells/template/cayley.nix
@@ -0,0 +1,44 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  name = "template-shell-cayley";
+  env  = buildEnv {
+    name  = name;
+    paths = buildInputs;
+  };
+
+  shellHook = ''
+    PROJECT_ROOT="/data/projects/private/configuration.nix/runtime/cayley"
+    SHELL_LOCK="$PROJECT_ROOT/shell.lock"
+    SHELL_NAME="${name}"
+
+    function finish {
+      ps | grep 'cayley' | grep -v 'grep' | awk '{ print $1 }' | xargs kill
+
+      rm -f "$SHELL_LOCK"
+    }
+
+    if [ ! -f "$SHELL_LOCK" ]; then
+      mkdir -p "$(dirname "$SHELL_LOCK")"
+      touch "$SHELL_LOCK"
+
+      SHELL_NAME="$SHELL_NAME|\[\e[1m\]master\[\e[0m\]"
+
+      cayley http \
+        --db=memstore --dbpath="" \
+        --assets="${cayley}/assets" \
+      2>/dev/null &
+
+      trap finish EXIT
+    fi
+
+    export ERL_AFLAGS="-kernel shell_history enabled"
+    export PS1="[$SHELL_NAME:\w]$ "
+  '';
+
+  cayley = pkgs.callPackage /data/projects/private/configuration.nix/packages/cayley {};
+
+  buildInputs = [
+    cayley
+  ];
+}
