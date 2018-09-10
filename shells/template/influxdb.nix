a464f547453c5b5734921ac620c6c7c22ece4c32
Marc Neudert <marc.neudert@gmail.com>
2018-09-10 19:52:29 +0200

    [shells] template: influxdb

---
+++ shells/template/influxdb.nix
@@ -0,0 +1,74 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  name = "template-shell-influxdb";
+  env  = buildEnv {
+    name  = name;
+    paths = buildInputs;
+  };
+
+  shellHook = ''
+    PROJECT_ROOT="/data/projects/private/configuration.nix/runtime/influxdb"
+    SHELL_LOCK="$PROJECT_ROOT/shell.lock"
+    SHELL_NAME="${name}"
+      
+    API_INFLUXDB='http://localhost:8086'
+    CONF_INFLUXDB="$PROJECT_ROOT/influxdb.conf"
+    LOG_INFLUXDB="$PROJECT_ROOT/influxdb.log"
+    PID_INFLUXDB="$PROJECT_ROOT/influxdb.pid"
+
+    function finish {
+      [ -f "$PID_INFLUXDB" ] && {
+        kill -TERM "$(cat "$PID_INFLUXDB")"
+        rm -f "$PID_INFLUXDB"
+      }
+
+      rm -f "$SHELL_LOCK"
+    }
+
+    function restart_influxdb {
+      [ -f "$PID_INFLUXDB" ] && {
+        kill -TERM "$(cat "$PID_INFLUXDB")"
+        rm -f "$PID_INFLUXDB"
+      }
+
+      influxd \
+          -config "$CONF_INFLUXDB" \
+          -pidfile "$PID_INFLUXDB" \
+      >/dev/null 2>$LOG_INFLUXDB &
+
+      until curl -s -o /dev/null "$API_INFLUXDB"; do
+        sleep 1
+      done
+    }
+
+    function setup_influxdb {
+      mkdir -p "$PROJECT_ROOT"
+
+      influxd config > "$CONF_INFLUXDB"
+
+      sed -i "s|/var/lib/influxdb|$PROJECT_ROOT|g" "$CONF_INFLUXDB"
+    }
+
+    if [ ! -f "$SHELL_LOCK" ]; then
+      mkdir -p "$(dirname "$SHELL_LOCK")"
+      touch "$SHELL_LOCK"
+
+      SHELL_NAME="$SHELL_NAME|\[\e[1m\]master\[\e[0m\]"
+
+      setup_influxdb
+      restart_influxdb
+
+      trap finish EXIT
+    fi
+
+    export ERL_AFLAGS="-kernel shell_history enabled"
+    export PS1="[$SHELL_NAME:\w]$ "
+  '';
+
+  influxdb = pkgs.callPackage /data/projects/private/configuration.nix/packages/influxdb {};
+
+  buildInputs = [
+    influxdb
+  ];
+}
