148d35ee3912334d83331c9efdd5c6b0d0fd9bc3
Marc Neudert <marc.neudert@gmail.com>
2019-02-15 17:44:16 +0100

    [packages] kibana-next: 7.0.0-alpha2 -> 7.0.0-beta1

--- packages/kibana-next/default.nix
+++ packages/kibana-next/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name    = "kibana-${version}";
-  version = "7.0.0-alpha2";
+  version = "7.0.0-beta1";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/kibana/kibana-oss-${version}-linux-x86_64.tar.gz";
-    sha256 = "09yryq1hxxap79lidg568wh2g0i3lgnbyd104r8gf7aryg49sw58";
+    sha256 = "0b5awnpqj7mk5hzm5grs4iyjvbhkbf89ir35p01v9w3qa8b7fjmd";
   };
 
   buildInputs = [ makeWrapper jre utillinux nodejs ];
