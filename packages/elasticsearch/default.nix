860d452c8d19ad2cafc121e83f108def3d431d0d
Marc Neudert <marc.neudert@gmail.com>
2019-02-20 18:09:15 +0100

    [packages] elasticsearch: 6.6.0 -> 6.6.1

--- packages/elasticsearch/default.nix
+++ packages/elasticsearch/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name    = "elasticsearch-${version}";
-  version = "6.6.0";
+  version = "6.6.1";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-${version}.tar.gz";
-    sha256 = "197h9paibi97wvxy091phddrnns4s65vav013i01givkzbn2wc2z";
+    sha256 = "0hjj1as23yxmkbgz2p3f2fd5gfvcvd97nxgfbnsrxj6wd3pk5g6d";
   };
 
   patches = [
