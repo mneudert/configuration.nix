9e62f9c1f3f7ca8ae517ef0c17cb8b42dc7c5a23
Marc Neudert <marc.neudert@gmail.com>
2019-03-12 18:11:44 +0100

    [packages] kibana: 6.6.1 -> 6.6.2

--- packages/kibana/default.nix
+++ packages/kibana/default.nix
@@ -3,20 +3,20 @@
 stdenv.mkDerivation rec {
   nodejs = pkgs.nodejs-10_x.overrideAttrs (oldAttrs: rec {
     name = "nodejs-${version}";
-    version = "10.15.1";
+    version = "10.15.2";
 
     src = fetchurl {
       url = "https://nodejs.org/dist/v${version}/node-v${version}.tar.xz";
-      sha256 = "0n68c4zjakdj6yzdc9fbrn0wghyslkya9sz1v6122i40zfwzfm8s";
+      sha256 = "0ncc27azpfrhc55n4j35wqcxbf7n42j0j07pq9dqjvh1rfkjvfxq";
     };
   });
 
   name    = "kibana-${version}";
-  version = "6.6.1";
+  version = "6.6.2";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/kibana/kibana-oss-${version}-linux-x86_64.tar.gz";
-    sha256 = "1b6gxjhcwhrr7vwfbla1aqfrn7g3cfa370pq5blpx71y3n3fbpvy";
+    sha256 = "0jrjbg63nxnpc2cx8m6z6fw28mj1yq0dw1whbykaxdwzhjcf0v96";
   };
 
   buildInputs = [ makeWrapper nodejs ];
