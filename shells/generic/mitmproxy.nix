886f28bd01aa2b0ee7c72d11e9675483f48543ef
Marc Neudert <marc.neudert@gmail.com>
2019-04-24 19:43:47 +0200

    [shells] generic: mitmproxy

---
+++ shells/generic/mitmproxy.nix
@@ -0,0 +1,17 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+  name = "generic-shell-mitmproxy";
+  env  = buildEnv {
+    name  = name;
+    paths = buildInputs;
+  };
+
+  shellHook = ''
+    export PS1="[generic:mitmproxy|\[\e[1m\]\w\[\e[0m\]]$ "
+  '';
+
+  buildInputs = [
+    mitmproxy
+  ];
+}
