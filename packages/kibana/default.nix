ecbfad3d53452945f2850bed88c8ba0e001a8e7b
Marc Neudert <marc.neudert@gmail.com>
2019-05-22 19:48:58 +0200

    [packages] kibana: 7.0.1 -> 7.1.0

--- packages/kibana/default.nix
+++ packages/kibana/default.nix
@@ -12,11 +12,11 @@ stdenv.mkDerivation rec {
   });
 
   name = "kibana-${version}";
-  version = "7.0.1";
+  version = "7.1.0";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/kibana/kibana-oss-${version}-linux-x86_64.tar.gz";
-    sha256 = "1x3gjc9xa03m4jfnl5vjxigzcnb8ysnhxgd8618v85x4l0010v38";
+    sha256 = "1zqkqixw9307qy64pf8rclmfvgd3m448h22dfnl9zx546flhl89y";
   };
 
   buildInputs = [ makeWrapper nodejs ];
