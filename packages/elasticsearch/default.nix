7737da328fc9e35edf3e3262be0f80400180ec20
Marc Neudert <marc.neudert@gmail.com>
2019-08-01 18:14:54 +0200

    [packages] elasticsearch: 7.2.1 -> 7.3.0

--- packages/elasticsearch/default.nix
+++ packages/elasticsearch/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "elasticsearch-${version}";
-  version = "7.2.1";
+  version = "7.3.0";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-${version}-linux-x86_64.tar.gz";
-    sha256 = "0d5rwqbrhilj5dcwkfrixkfm556kfsndfwlgbfz863g78b97nnlj";
+    sha256 = "1cmh18h3571dhplz7wyjc0mv8741pg144mpzapcq20a8xgqlw1v4";
   };
 
   patches = [
