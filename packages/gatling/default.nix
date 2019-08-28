7cf8ba84be107c4ddd1f62dd0c8fc0150b5130e9
Marc Neudert <marc.neudert@gmail.com>
2019-08-28 19:21:23 +0200

    [packages] gatling: 3.2.0 -> 3.2.1

--- packages/gatling/default.nix
+++ packages/gatling/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "gatling-${version}";
-  version = "3.2.0";
+  version = "3.2.1";
 
   src = fetchzip {
     url = "https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/${version}/gatling-charts-highcharts-bundle-${version}-bundle.zip";
-    sha256 = "04ihckf34wlal56n4frmny4ghj7yvgmg31rwrzqvn1d5hm7zgxy2";
+    sha256 = "179jln7j3gydrnp1f0ql2ycra3nvzy52xrkp7j1nlp5lgcsaxwws";
   };
 
   buildInputs = [ makeWrapper jdk ];
