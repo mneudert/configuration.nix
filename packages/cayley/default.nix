8e455e47faa80e9b61d695169166eaf0961d2bb1
Marc Neudert <marc.neudert@gmail.com>
2019-10-17 13:46:40 +0200

    [packages] cayley: 0.7.5 -> 0.7.7

--- packages/cayley/default.nix
+++ packages/cayley/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "cayley-${version}";
-  version = "0.7.5";
+  version = "0.7.7";
 
   src = fetchurl {
     url = "https://github.com/cayleygraph/cayley/releases/download/v${version}/cayley_${version}_linux_amd64.tar.gz";
-    sha256 = "1bq2gqbjljc0ws5lnfjvvhwhnykpq6k61mv2ccfm7hcqd9wgmhb9";
+    sha256 = "1hm69hhrw5x9qrd8174lw619v8kb42dbrm3xh7m31va9lpsllq62";
   };
 
   installPhase = ''
--- shells/project/caylir_showcase.nix
+++ shells/project/caylir_showcase.nix
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
 
       SHELL_NAME="$SHELL_NAME|\[\e[1m\]master\[\e[0m\]"
 
-      cayley http --db=memstore --dbpath="" 2>/dev/null &
+      cayley http 2>/dev/null &
 
       trap finish EXIT
     fi
