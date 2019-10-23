970ef07bbb085bf710dadfcdf875d055d44b6dff
Marc Neudert <marc.neudert@gmail.com>
2019-10-23 18:40:36 +0200

    [packages] kibana: 7.4.0 -> 7.4.1

--- packages/kibana/default.nix
+++ packages/kibana/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "kibana-${version}";
-  version = "7.4.0";
+  version = "7.4.1";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/kibana/kibana-oss-${version}-linux-x86_64.tar.gz";
-    sha256 = "14msp8zqgcrfrl2ca7a1l3r4wpahipra81l6c8zni11wcl79rw6b";
+    sha256 = "1k77i7i5lh2m250fxsqakaki5rghl1hjfn62nbdla6g58lpvbykh";
   };
 
   installPhase = ''
