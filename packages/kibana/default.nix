df1579a9fa66523b8551bcb1953f8c4ae8e70191
Marc Neudert <marc.neudert@gmail.com>
2020-02-11 18:45:45 +0100

    [packages] kibana: 7.5.2 -> 7.6.0

--- packages/kibana/default.nix
+++ packages/kibana/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "kibana-${version}";
-  version = "7.5.2";
+  version = "7.6.0";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/kibana/kibana-oss-${version}-linux-x86_64.tar.gz";
-    sha256 = "00zvhb92g639r77awm07xs4xhx3y8mdrgfwz43jlw3z5ysv497yv";
+    sha256 = "0fmf2bq1m3f6wvh6xpywwclkarqwsdd3xm225hsxpslgzdjqbiw4";
   };
 
   installPhase = ''
