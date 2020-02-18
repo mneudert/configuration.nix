95a488d663cee54b5359fa36e19d79949f57ef4e
Marc Neudert <marc.neudert@gmail.com>
2020-02-18 20:07:36 +0100

    [shells] Use shell name from variable in prompt

--- shells/project/phoenix.nix
+++ shells/project/phoenix.nix
@@ -8,12 +8,14 @@ stdenv.mkDerivation rec {
   };
 
   shellHook = ''
+    SHELL_NAME="project:phoenix"
+
     export SHELL_DATA_DIR="$HOME/.nix-shells-data/${name}"
 
     export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$SHELL_DATA_DIR/erlang-history\"'"
     export HEX_HOME="$SHELL_DATA_DIR/hex"
     export MIX_HOME="$SHELL_DATA_DIR/mix"
-    export PS1="[project:phoenix|\[\e[1m\]\w\[\e[0m\]]$ "
+    export PS1="[$SHELL_NAME|\[\e[1m\]\w\[\e[0m\]]$ "
   '';
 
   elixir = pkgs.callPackage /data/projects/private/configuration.nix/packages/elixir-1.10 {};
