fb17ed46ca56286da293a14edc371ba2ebeca2d2
Marc Neudert <marc.neudert@gmail.com>
2020-02-18 20:07:16 +0100

    [shells] Fix shell prompt

--- shells/project/ecto_ip_range.nix
+++ shells/project/ecto_ip_range.nix
@@ -60,7 +60,7 @@ stdenv.mkDerivation rec {
     export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$SHELL_DATA_DIR/erlang-history\"'"
     export HEX_HOME="$SHELL_DATA_DIR/hex"
     export MIX_HOME="$SHELL_DATA_DIR/mix"
-    export PS1="[project:phoenix|\[\e[1m\]\w\[\e[0m\]]$ "
+    export PS1="[$SHELL_NAME|\[\e[1m\]\w\[\e[0m\]]$ "
   '';
 
   elixir = pkgs.callPackage /data/projects/private/configuration.nix/packages/elixir-1.10 {};
