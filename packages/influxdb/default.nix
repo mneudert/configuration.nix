0e3835ab75342f38276df3c3a5d048082e497c11
Marc Neudert <marc.neudert@gmail.com>
2020-04-12 13:36:13 +0200

    [packages] influxdb: 1.7.10 -> 1.8.0

--- packages/influxdb/default.nix
+++ packages/influxdb/default.nix
@@ -1,20 +1,16 @@
-{ lib, buildGoPackage, fetchFromGitHub }:
+{ lib, buildGoModule, fetchFromGitHub }:
 
-buildGoPackage rec {
+buildGoModule rec {
   name = "influxdb-${version}";
-  version = "1.7.10";
+  version = "1.8.0";
+  modSha256 = "058gg66bvxxk1nia9gqjvimvwvpcbl5xq0y5y58yyx2hv0l3xl0c";
 
   src = fetchFromGitHub {
     owner = "influxdata";
     repo = "influxdb";
     rev = "v${version}";
-    sha256 = "0139h9ygilg4r3b9qdqbxdrn9fw163ghm63hfbh6p8z3aymd346f";
+    sha256 = "111n36xifmd644xp80imqxx61nlap6fdwx1di2qphlqb43z99jrq";
   };
-
-  goDeps =  ./deps.nix;
-  goPackagePath = "github.com/influxdata/influxdb";
-
-  excludedPackages = "test";
 
   patchPhase = ''
     sed -i -e 's/version = "unknown"/version = "${version}"/g' ./cmd/influxd/main.go
