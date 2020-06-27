e3c9796e1dc93943f36908f9719a93b8ba4f012d
Marc Neudert <marc.neudert@gmail.com>
2020-06-27 11:05:41 +0200

    [packages] Provide non-OSS Kibana package

--- packages/kibana/default.nix
+++ packages/kibana/default.nix
@@ -1,12 +1,19 @@
-{ stdenv, fetchurl, patchelf }:
+{ stdenv, fetchurl, patchelf, oss ? true }:
 
-stdenv.mkDerivation rec {
-  name = "kibana-${version}";
+let
+  package = if oss then "kibana-oss" else "kibana";
+  hash =
+    if oss then
+      "0rnw4bkmiygy19wx57csy72n7piwzksh623j0nkqilbivwvajabi"
+    else
+      "0xnh07n894f170ahawcg03jm3xk4qpjjbfwkvd955vdgihpy60gh";
+in stdenv.mkDerivation rec {
+  name = "${package}-${version}";
   version = "7.8.0";
 
   src = fetchurl {
-    url = "https://artifacts.elastic.co/downloads/kibana/kibana-oss-${version}-linux-x86_64.tar.gz";
-    sha256 = "0rnw4bkmiygy19wx57csy72n7piwzksh623j0nkqilbivwvajabi";
+    url = "https://artifacts.elastic.co/downloads/kibana/${package}-${version}-linux-x86_64.tar.gz";
+    sha256 = hash;
   };
 
   installPhase = ''
--- shells/template/kibana-oss.nix
+++ shells/template/kibana-oss.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
-  name = "template-shell-kibana";
+  name = "template-shell-kibana-oss";
   env = buildEnv {
     name = name;
     paths = buildInputs;
@@ -9,9 +9,9 @@ stdenv.mkDerivation rec {
 
   shellHook = ''
     PROJECT_ROOT="$(pwd)"
-    PID_KIBANA="$PROJECT_ROOT/runtime/kibana/kibana.pid"
-    SHELL_LOCK="$PROJECT_ROOT/runtime/kibana/shell.lock"
-    SHELL_NAME="template:kibana"
+    PID_KIBANA="$PROJECT_ROOT/runtime/kibana-oss/kibana.pid"
+    SHELL_LOCK="$PROJECT_ROOT/runtime/kibana-oss/shell.lock"
+    SHELL_NAME="template:kibana-oss"
 
     function finish {
       [ -f "$PID_KIBANA" ] && {
@@ -23,11 +23,11 @@ stdenv.mkDerivation rec {
     }
 
     function setup_kibana {
-      mkdir -p runtime/kibana
+      mkdir -p runtime/kibana-oss
 
       sed "s|{{PATH_PROJECT}}|$PROJECT_ROOT|g" \
-          runtime/etc/kibana.yml \
-      > runtime/kibana/kibana.yml
+          runtime/etc/kibana-oss.yml \
+      > runtime/kibana-oss/kibana.yml
     }
 
     if [ ! -f "$SHELL_LOCK" ]; then
@@ -38,9 +38,9 @@ stdenv.mkDerivation rec {
 
       setup_kibana
 
-      BABEL_CACHE_PATH="$PROJECT_ROOT/runtime/kibana/.babelcache.json" \
-          DATA_PATH="$PROJECT_ROOT/runtime/kibana/" \
-          kibana -c "$PROJECT_ROOT/runtime/kibana/kibana.yml" &
+      BABEL_CACHE_PATH="$PROJECT_ROOT/runtime/kibana-oss/.babelcache.json" \
+          DATA_PATH="$PROJECT_ROOT/runtime/kibana-oss/" \
+          kibana -c "$PROJECT_ROOT/runtime/kibana-oss/kibana.yml" &
 
       trap finish EXIT
     fi
@@ -48,7 +48,7 @@ stdenv.mkDerivation rec {
     export PS1="[$SHELL_NAME|\[\e[1m\]\w\[\e[0m\]]$ "
   '';
 
-  kibana = pkgs.callPackage /data/projects/private/configuration.nix/packages/kibana { oss = false; };
+  kibana = pkgs.callPackage /data/projects/private/configuration.nix/packages/kibana {};
 
   buildInputs = [
     kibana
