e4a13056955dc735b4c9a2faba9eba78a11ba2b0
Marc Neudert <marc.neudert@gmail.com>
2020-08-08 13:12:40 +0200

    [packages] influxdb2: 2.0.0-beta.15 -> 2.0.0-beta.16

--- packages/influxdb2/default.nix
+++ packages/influxdb2/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "influxdb-${version}";
-  version = "2.0.0-beta.15";
+  version = "2.0.0-beta.16";
 
   src = fetchurl {
     url = "https://dl.influxdata.com/influxdb/releases/influxdb_${version}_linux_amd64.tar.gz";
-    sha256 = "1p4b71n23phsiqq0kg9vnqc337l2gkwbydcqnrvb66w9r3cjz90v";
+    sha256 = "0higzzp4sqqybv8mk4xqapwq8iw4v2lwvd1xbr6i8pkzws5h88xd";
   };
 
   installPhase = ''
