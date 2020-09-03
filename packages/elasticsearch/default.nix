7604dfea0e6da9b2e8af5bc65a8a5bcdac73423c
Marc Neudert <marc.neudert@gmail.com>
2020-09-03 18:38:01 +0200

    [packages] elasticsearch: 7.9.0 -> 7.9.1

--- packages/elasticsearch/default.nix
+++ packages/elasticsearch/default.nix
@@ -5,12 +5,12 @@ let
   package = if oss then "elasticsearch-oss" else "elasticsearch";
   hash =
     if oss then
-      "1x51l7c9705jrqrb5mwh4pbypszm1f8s2m71apv9xspnpzj4gmq7"
+      "0swxxyzgzwplnl407wg4kvkxl8sn0n1vx7y3rzpmgknkc93naazw"
     else
-      "0yd3k6hsr24idv1z99slrpr413pmdiql6nxxn28wdl6hihqfg34z";
+      "0g3f9ix4839kcr7j292mfzj4c9l1rq3l1xk6hg893jnk55p3md51";
 in stdenv.mkDerivation rec {
   name = "${package}-${version}";
-  version = "7.9.0";
+  version = "7.9.1";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/elasticsearch/${package}-${version}-linux-x86_64.tar.gz";
