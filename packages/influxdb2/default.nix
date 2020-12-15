fd6847bf94084561aca08e34ada7132bc75a24b5
Marc Neudert <marc.neudert@gmail.com>
2020-12-15 17:31:39 +0100

    [packages] influxdb2: 2.0.2 -> 2.0.3

--- packages/influxdb2/default.nix
+++ packages/influxdb2/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "influxdb-${version}";
-  version = "2.0.2";
+  version = "2.0.3";
 
   src = fetchurl {
-    url = "https://dl.influxdata.com/influxdb/releases/influxdb-${version}_linux_amd64.tar.gz";
-    sha256 = "0c9zk26izqzylylk0aacxs5vl6aa02cc402s3sm42ra4vh8ls5h2";
+    url = "https://dl.influxdata.com/influxdb/releases/influxdb2-${version}_linux_amd64.tar.gz";
+    sha256 = "1by4z8zm7c8378lzlhq7j86cq8rlz7mq9y1932zf01j4cdnhn7yh";
   };
 
   installPhase = ''
