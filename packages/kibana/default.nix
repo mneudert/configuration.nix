64169e27a17dc20ab72447b2d090c2f06778338f
Marc Neudert <marc.neudert@gmail.com>
2021-01-15 19:14:24 +0100

    [packages] kibana: 7.10.1 -> 7.10.2

--- packages/kibana/default.nix
+++ packages/kibana/default.nix
@@ -4,12 +4,12 @@ let
   package = if oss then "kibana-oss" else "kibana";
   hash =
     if oss then
-      "089axbdsr5r5gj0jvv2gmh6vfrzw3nbcxzbs2cjmdf2fb8m9sfm2"
+      "050rhx82rqpgqssp1rdflz1ska3f179kd2k2xznb39614nk0m6gs"
     else
-      "19iqy3jhkk46dkdm5igg0xxhkf23g28sj4zdk25zjj9yrn1z377v";
+      "06p0v39ih606mdq2nsdgi5m7y1iynk9ljb9457h5rrx6jakc2cwm";
 in stdenv.mkDerivation rec {
   name = "${package}-${version}";
-  version = "7.10.1";
+  version = "7.10.2";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/kibana/${package}-${version}-linux-x86_64.tar.gz";
