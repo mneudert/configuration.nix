b11c12fec7a6c378fffe2446896754b83d491176
Marc Neudert <marc.neudert@gmail.com>
2021-04-30 16:51:32 +0200

    [packages] influxdb2: 2.0.5 -> 2.0.6

--- packages/influxdb2/default.nix
+++ packages/influxdb2/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   name = "influxdb-${version}";
-  version = "2.0.5";
+  version = "2.0.6";
 
   src = fetchurl {
     url = "https://dl.influxdata.com/influxdb/releases/influxdb2-${version}-linux-amd64.tar.gz";
-    sha256 = "1f9wcin6zsmzm42lpfkhwyk9cd65nck27xjk8jzawl2d7kwy6qfz";
+    sha256 = "03f1xccglc0id4n49y7abmdh21s014dz4yr68h75l28dwapvajpj";
   };
 
   installPhase = ''
