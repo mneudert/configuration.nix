404c86bc157d7af9d46feb63dca06175a7b7f1ef
Marc Neudert <marc.neudert@gmail.com>
2021-05-29 12:43:22 +0200

    [packages] influxdb: 1.8.5 -> 1.8.6

--- packages/influxdb/default.nix
+++ packages/influxdb/default.nix
@@ -2,14 +2,14 @@
 
 buildGoModule rec {
   name = "influxdb-${version}";
-  version = "1.8.5";
+  version = "1.8.6";
   vendorSha256 = "16lbxzn2jbwqdc6axpzxy1gckq520bdw389k8qfvdq65rapb1fxp";
 
   src = fetchFromGitHub {
     owner = "influxdata";
     repo = "influxdb";
     rev = "v${version}";
-    sha256 = "0n3yr84lf75af7zs6ld98w0sarihb1981h6chj4klyllqi705ad8";
+    sha256 = "18436i9ixpjlbqqlpgxxm6ahbnwlzkd51hfn487fam8kxjp1v7xz";
   };
 
   doCheck = false;
