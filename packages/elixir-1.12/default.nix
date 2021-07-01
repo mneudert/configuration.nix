df2290d6e514d90681fa8c72bb6cc46c9b4f9bb1
Marc Neudert <marc.neudert@gmail.com>
2021-07-01 18:59:42 +0200

    [packages] elixir-1.12: 1.12.1 -> 1.12.2

--- packages/elixir-1.12/default.nix
+++ packages/elixir-1.12/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "elixir-${version}";
-  version = "1.12.1";
+  version = "1.12.2";
 
   src = fetchFromGitHub {
     owner = "elixir-lang";
     repo = "elixir";
     rev = "v${version}";
-    sha256 = "1f8b63x2klhdz1fq1dgbrqs7x6rq309abzq48gicmd0vprfhc641";
+    sha256 = "1rwmwnqxhjcdx9niva9ardx90p1qi4axxh72nw9k15hhlh2jy29x";
   };
 
   buildInputs = [ erlang makeWrapper ];
