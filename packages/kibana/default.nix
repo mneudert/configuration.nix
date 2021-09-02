82d38c3a3a437b58fd75eac471616ac13cbde920
Marc Neudert <marc.neudert@gmail.com>
2021-09-02 18:49:28 +0200

    [packages] kibana: 7.14.0 -> 7.14.1

--- packages/kibana/default.nix
+++ packages/kibana/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   name = "kibana-${version}";
-  version = "7.14.0";
+  version = "7.14.1";
 
   src = fetchurl {
     url =
       "https://artifacts.elastic.co/downloads/kibana/kibana-${version}-linux-x86_64.tar.gz";
-    sha256 = "03cpm8fvmsj77b75csa0390mqiggwc3im9ycpgd8nvl6ly520bx0";
+    sha256 = "0msjmndfsw0v4nm47iac2nhmx4pd0sh0riq71qidzl0mp1si7cl8";
   };
 
   installPhase = ''
