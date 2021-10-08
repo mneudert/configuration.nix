b814901446b02a3533d91cc76a621f362ffb79b9
Marc Neudert <marc.neudert@gmail.com>
2021-10-08 18:18:38 +0200

    [packages] Remove misspell

--- hosts/cubicle/custom-packages.nix
+++ hosts/cubicle/custom-packages.nix
@@ -1,11 +1,10 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ bash-todo gitcheck misspell ];
+  environment.systemPackages = with pkgs; [ bash-todo gitcheck ];
 
   nixpkgs.config.packageOverrides = pkgs: rec {
     bash-todo = pkgs.callPackage ../../packages/bash-todo { };
     gitcheck = pkgs.callPackage ../../packages/gitcheck { };
-    misspell = pkgs.callPackage ../../packages/misspell { };
   };
 }
