8d3ba18c752f9271d87bcd3a9c477eebf2e6e228
Marc Neudert <marc.neudert@gmail.com>
2021-12-01 17:16:14 +0100

    [shells] Add missing SHELL_DATA_DIR configuration

--- shells/project/hidrd.nix
+++ shells/project/hidrd.nix
@@ -8,13 +8,17 @@ stdenv.mkDerivation rec {
   };
 
   shellHook = ''
+    export SHELL_DATA_DIR="/data/nix-shells/${name}"
+
     if ! grep -q 'usbmon' <( lsmod ); then
       echo 'Configuring usbmon...'
 
       sudo modprobe usbmon && sudo setfacl -m u:$USER:r /dev/usbmon*
     fi
 
-    export ERL_AFLAGS="-kernel shell_history enabled"
+    export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$SHELL_DATA_DIR/erlang-history\"'"
+    export HEX_HOME="$SHELL_DATA_DIR/hex"
+    export MIX_HOME="$SHELL_DATA_DIR/mix"
     export PS1="[project:hidrd|\[\e[1m\]\w\[\e[0m\]]$ "
   '';
 
