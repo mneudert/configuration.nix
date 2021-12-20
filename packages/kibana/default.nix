095b53dde94fb975f35b725f052aa1f99a6bfd41
Marc Neudert <marc.neudert@gmail.com>
2021-12-20 17:40:34 +0100

    [packages] kibana: 7.16.1 -> 7.16.2

--- packages/kibana/default.nix
+++ packages/kibana/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   name = "kibana-${version}";
-  version = "7.16.1";
+  version = "7.16.2";
 
   src = fetchurl {
     url =
       "https://artifacts.elastic.co/downloads/kibana/kibana-${version}-linux-x86_64.tar.gz";
-    sha256 = "0jivwsrq31n0qfznrsjfsn65sg3wpbd990afn2wzjnj4drq7plz6";
+    sha256 = "0i936jcplg3nwp2vfnw9whrq7jxj52b5g8ryl5ccrpj5wwv8icpf";
   };
 
   installPhase = ''
