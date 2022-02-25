66792ca75cd20a62f888a60740d6ed46fbd987e9
Marc Neudert <marc.neudert@gmail.com>
2022-02-25 17:57:39 +0100

    [packages] Use gnu-cobol from upstream

--- shells/generic/cobol.nix
+++ shells/generic/cobol.nix
@@ -11,9 +11,5 @@ stdenv.mkDerivation rec {
     export PS1="[generic:cobol|\[\e[1m\]\w\[\e[0m\]]$ "
   '';
 
-  gnu-cobol =
-    pkgs.callPackage /data/projects/private/configuration.nix/packages/gnu-cobol
-    { };
-
   buildInputs = [ gnu-cobol ];
 }
