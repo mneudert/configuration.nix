1f02a7eda6f7d1371c34ee84a1fb2adf48a0046c
Marc Neudert <marc.neudert@gmail.com>
2022-06-01 18:04:14 +0200

    [cubicle] Upgrade to NixOS 22.05

--- hosts/cubicle/unfree-packages.nix
+++ hosts/cubicle/unfree-packages.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ idea.phpstorm sublime3 vscode ];
+  environment.systemPackages = with pkgs; [ jetbrains.phpstorm sublime3 vscode ];
 }
