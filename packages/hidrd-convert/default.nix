c88b959abf94966397cf1320cafc2cc746896a5b
Marc Neudert <marc.neudert@gmail.com>
2022-06-05 11:30:56 +0200

    [packages] hidrd-convert: dev-2017-08-02 -> dev-2019-06-03

--- packages/hidrd-convert/default.nix
+++ packages/hidrd-convert/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "hidrd-convert-${version}";
-  version = "dev-2017-08-02";
+  version = "dev-2019-06-03";
 
   src = fetchFromGitHub {
     owner = "DIGImend";
     repo = "hidrd";
-    rev = "7e94881a6059a824efaed41301c4a89a384d86a2";
-    hash = "sha256-d5tIz+07XkXxEorLtmaaZWZcIxsI1jx1JGpQTp/Ar+Q=";
+    rev = "6c0ed39708a5777ac620f902f39c8a0e03eefe4e";
+    hash = "sha256-JK1llvWN8Vzu2YDf/wZJ41F81/034/Big7dmC5bB0OY=";
   };
 
   preConfigure = ''
