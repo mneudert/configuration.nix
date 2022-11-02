38015601bf19768b89c0558cc36fa145f37b711a
Marc Neudert <marc.neudert@gmail.com>
2022-11-02 16:08:59 +0100

    [packages] influxdb2-client: 2.4.0 -> 2.5.0

--- packages/influxdb2-client/default.nix
+++ packages/influxdb2-client/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   name = "influxdb2-client-${version}";
-  version = "2.4.0";
+  version = "2.5.0";
 
   src = fetchurl {
     url =
       "https://dl.influxdata.com/influxdb/releases/influxdb2-client-${version}-linux-amd64.tar.gz";
-    hash = "sha256-hereiqMt6II9mBbuz0fC+WIIg/U8uXXbaqsG55bXFcM=";
+    hash = "sha256-45e2WbOB9/XogebM2bEzZQuQGEmHw93c6/zQ+Mo8WlY=";
   };
 
   installPhase = ''
