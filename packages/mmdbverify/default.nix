31917a88b40a3e98abbf4311673841b059684b8f
Marc Neudert <marc.neudert@gmail.com>
2023-04-27 20:12:01 +0200

    [packages] mmdbverify: git.2022-08-30 -> git.2023-03-16

--- packages/mmdbverify/default.nix
+++ packages/mmdbverify/default.nix
@@ -2,14 +2,14 @@
 
 buildGoModule rec {
   name = "mmdbverify-${version}";
-  version = "git.2022-08-30";
+  version = "git.2023-03-16";
   vendorSha256 = "sha256-spwlkP4Qsz7+GeX9Ol1fLRcotLVuH33ufoYjo3q7Nsw=";
 
   src = fetchFromGitHub {
     owner = "maxmind";
     repo = "mmdbverify";
-    rev = "5072d9405ca81cb4d13eb484a9277b7056ede3ae";
-    hash = "sha256-KWY/ft74QCmFrA8edBEyApVfYkiimREoWrrSDY1kc8Q=";
+    rev = "a5191b352a686533f1d64c29d9ecb0923b5df593";
+    hash = "sha256-wsqHrNptYB/RqQthI6aCQ2Pa/79uzYuIQG782vs4vHs=";
   };
 
   doCheck = false;
