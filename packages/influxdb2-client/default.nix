39766fa302a2a77d8c07b03d116c7c09a1487df8
Marc Neudert <marc.neudert@gmail.com>
2023-04-29 10:40:44 +0200

    [packages] influxdb2-client: 2.7.1 -> 2.7.3

--- packages/influxdb2-client/default.nix
+++ packages/influxdb2-client/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   name = "influxdb2-client-${version}";
-  version = "2.7.1";
+  version = "2.7.3";
 
   src = fetchurl {
     url =
-      "https://dl.influxdata.com/influxdb/releases/influxdb2-client-${version}_linux_amd64.tar.gz";
-    hash = "sha256-61kv7pvdvEH8eC/2KWyPG4rFm2A56rCSNs5TodT7KGc=";
+      "https://dl.influxdata.com/influxdb/releases/influxdb2-client-${version}-linux-amd64.tar.gz";
+    hash = "sha256-ombzBFR0Y7a8eIa/ReN9JSvMDOsxVquNeMUlYVWPv+Y=";
   };
 
   setSourceRoot = "sourceRoot=`pwd`";
