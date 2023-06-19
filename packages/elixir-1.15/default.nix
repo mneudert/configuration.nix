0785f278f4a89d721ea621f746af2088020f94a5
Marc Neudert <marc.neudert@gmail.com>
2023-06-19 16:36:10 +0200

    [packages] elixir-1.15: init at 1.15.0

---
+++ packages/elixir-1.15/default.nix
@@ -0,0 +1,47 @@
+{ pkgs, lib, stdenv, fetchFromGitHub, erlang, makeWrapper, coreutils, curl, bash
+}:
+
+stdenv.mkDerivation rec {
+  name = "elixir-${version}";
+  version = "1.15.0";
+
+  src = fetchFromGitHub {
+    owner = "elixir-lang";
+    repo = "elixir";
+    rev = "v${version}";
+    hash = "sha256-o5MfA0UG8vpnPCH1EYspzcN62yKZQcz5uVUY47hOL9w=";
+  };
+
+  buildInputs = [ erlang makeWrapper ];
+
+  LOCALE_ARCHIVE = lib.optionalString stdenv.isLinux
+    "${pkgs.glibcLocales}/lib/locale/locale-archive";
+
+  LANG = "en_US.UTF-8";
+  LC_TYPE = "en_US.UTF-8";
+
+  buildFlags = "ERL_COMPILER_OPTIONS=debug_info";
+
+  preBuild = ''
+    patchShebangs lib/elixir/scripts/generate_app.escript || true
+
+    substituteInPlace Makefile --replace "/usr/local" $out
+    echo -n $version > VERSION
+  '';
+
+  postFixup = ''
+    # Elixir binaries are shell scripts which run erl. Add some stuff
+    # to PATH so the scripts can run without problems.
+
+    for f in $out/bin/*; do
+     b=$(basename $f)
+      if [ $b == "mix" ]; then continue; fi
+      wrapProgram $f \
+        --prefix PATH ":" "${lib.makeBinPath [ erlang coreutils curl bash ]}" \
+        --set CURL_CA_BUNDLE /etc/ssl/certs/ca-certificates.crt
+    done
+
+    substituteInPlace $out/bin/mix \
+          --replace "/usr/bin/env elixir" "${coreutils}/bin/env elixir"
+  '';
+}
