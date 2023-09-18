9076ef3d7683b7a759d0d17a5ae0d1104d715d3f
Marc Neudert <marc.neudert@gmail.com>
2023-09-18 16:45:20 +0200

    [packages] postgresql.ip4r: 2.4.1 -> 2.4.2

--- packages/postgresql/ip4r/default.nix
+++ packages/postgresql/ip4r/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "postgresql_ip4r-${version}";
-  version = "2.4.1";
+  version = "2.4.2";
 
   src = fetchFromGitHub {
     owner = "RhodiumToad";
     repo = "ip4r";
     rev = version;
-    hash = "sha256-smK3LkgQS3HFbxxGXg0vLuHjWA58brjgTXL++SGu0uY=";
+    hash = "sha256-3chAD4f4A6VlXVSI0kfC/ANcnFy4vBp4FZpT6QRAueQ=";
   };
 
   buildInputs = [ postgresql ];
