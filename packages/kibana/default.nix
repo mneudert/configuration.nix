8fb509456758ae5bdd5c6238b3ae655b8322af04
Marc Neudert <marc.neudert@gmail.com>
2023-09-21 18:01:01 +0200

    [packages] kibana: 8.10.1 -> 8.10.2

--- packages/kibana/default.nix
+++ packages/kibana/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   name = "kibana-${version}";
-  version = "8.10.1";
+  version = "8.10.2";
 
   src = fetchurl {
     url =
       "https://artifacts.elastic.co/downloads/kibana/kibana-${version}-linux-x86_64.tar.gz";
-    hash = "sha256-0ozg5mAgkV9PfQMx+5aUpsg/2XNABw2/FCW63amN89Q=";
+    hash = "sha256-qey8CEkO/6oK7G8blb2zmBGZVo5CZfz+tDUn9dOUiSI=";
   };
 
   installPhase = ''
