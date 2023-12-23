5d08384258b6fa5bf6ecb964485fe3f3d512d0e7
Marc Neudert <marc.neudert@gmail.com>
2023-12-23 11:42:58 +0100

    [shells] Bump default Elixir version to 1.16.x

--- shells/project/hidrd.nix
+++ shells/project/hidrd.nix
@@ -23,7 +23,7 @@ stdenv.mkDerivation rec {
   '';
 
   elixir = pkgs.callPackage
-    /data/projects/private/configuration.nix/packages/elixir-1.15 { };
+    /data/projects/private/configuration.nix/packages/elixir-1.16 { };
   hidrd-convert = pkgs.callPackage
     /data/projects/private/configuration.nix/packages/hidrd-convert { };
 
