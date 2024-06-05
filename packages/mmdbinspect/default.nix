84db23959768319d63f0f643c33f13163df9bfb9
Marc Neudert <marc.neudert@gmail.com>
2024-06-05 15:05:18 +0200

    [packages] mmdbinspect: 0.2.0 -> git.2024-06-05

--- packages/mmdbinspect/default.nix
+++ packages/mmdbinspect/default.nix
@@ -1,17 +1,16 @@
-{ stdenv, fetchurl }:
+{ lib, buildGoModule, fetchFromGitHub }:
 
-stdenv.mkDerivation rec {
+buildGoModule rec {
   name = "mmdbinspect-${version}";
-  version = "0.2.0";
+  version = "git.2024-06-05";
+  vendorHash = "sha256-c/YRssKIme7BO4slDLCSfa8ap4f6qSfW2eDaH1xaD0I=";
 
-  src = fetchurl {
-    url =
-      "https://github.com/maxmind/mmdbinspect/releases/download/v${version}/mmdbinspect_${version}_linux_amd64.tar.gz";
-    sha256 = "sha256-hXdIctmUUJsJ1qyXmpuDMVnOeXDoQoMqpRCo0j1fFXc=l";
+  src = fetchFromGitHub {
+    owner = "maxmind";
+    repo = "mmdbinspect";
+    rev = "8769f5e9951a0b932c99cf4034fe248add51171c";
+    hash = "sha256-Own/wpcZ4O0S8yb99I5zuGZ1sdxlsD0VAIoTgRwSy5A=";
   };
 
-  installPhase = ''
-    mkdir -p $out/bin/
-    cp -R mmdbinspect $out/bin/
-  '';
+  doCheck = false;
 }
