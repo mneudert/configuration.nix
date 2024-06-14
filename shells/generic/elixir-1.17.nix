9005463e52cb88104594da715f2d275eb2dbb443
Marc Neudert <marc.neudert@gmail.com>
2024-06-14 11:32:11 +0200

    [packages] elixir-1.17: init at 1.17.0

---
+++ shells/generic/elixir-1.17.nix
@@ -0,0 +1,23 @@
+with import <nixpkgs> { };
+
+stdenv.mkDerivation rec {
+  name = "generic-shell-elixir-1.17";
+  env = buildEnv {
+    name = name;
+    paths = buildInputs;
+  };
+
+  shellHook = ''
+    export SHELL_DATA_DIR="/data/nix-shells/${name}"
+
+    export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$SHELL_DATA_DIR/erlang-history\"'"
+    export HEX_HOME="$SHELL_DATA_DIR/hex"
+    export MIX_HOME="$SHELL_DATA_DIR/mix"
+    export PS1="[generic:elixir-1.17|\[\e[1m\]\w\[\e[0m\]]$ "
+  '';
+
+  elixir = pkgs.callPackage
+    /data/projects/private/configuration.nix/packages/elixir-1.17 { };
+
+  buildInputs = [ glibcLocales elixir erlang ];
+}
