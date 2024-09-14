ca29844023ec4fbfec7411ea25e887d2ca91ee42
Marc Neudert <marc.neudert@gmail.com>
2024-09-14 11:23:24 +0200

    [packages] mmdbinspect: git.2024-08-14 -> git.2024-09-13

--- packages/mmdbinspect/default.nix
+++ packages/mmdbinspect/default.nix
@@ -2,14 +2,14 @@
 
 buildGoModule rec {
   name = "mmdbinspect-${version}";
-  version = "git.2024-08-14";
+  version = "git.2024-09-13";
   vendorHash = "sha256-MuTZmuVXerzo2+NgMHvMPysvWo90M7iNbBs5npj+KTw=";
 
   src = fetchFromGitHub {
     owner = "maxmind";
     repo = "mmdbinspect";
-    rev = "75fb13e176b40d84e867049dc45d0ba1fb251c07";
-    hash = "sha256-0EuU0LO2qE2HUFMyxmu3C7HG4n3j++ims4Zgl/5CxY8=";
+    rev = "90021b0d1a49af213c21bd50c6321da39610919f";
+    hash = "sha256-7muOd5xHrHyIpxAHRZEc1RSzsn6P7RTyh8JBfOpw3fw=";
   };
 
   doCheck = false;
