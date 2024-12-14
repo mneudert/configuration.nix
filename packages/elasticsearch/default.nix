eec9445c3ff6fd3dae5cc73fc5d8197f1297006e
Marc Neudert <marc.neudert@gmail.com>
2024-12-14 10:17:56 +0100

    [packages] elasticsearch: 8.16.1 -> 8.17.0

--- packages/elasticsearch/default.nix
+++ packages/elasticsearch/default.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   name = "elasticsearch-${version}";
-  version = "8.16.1";
+  version = "8.17.0";
 
   src = fetchurl {
     url =
       "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${version}-linux-x86_64.tar.gz";
-    hash = "sha256-hq9si7rcYSYnCFIcB4F19/yAtGmxbk2qHRGA1J5qlVA=";
+    hash = "sha256-j/L9peECNExsOY/mxs3bqmHI0rRomNbIBvvqiphp/S8=";
   };
 
   patches = [ ./elasticsearch-env.patch ];
