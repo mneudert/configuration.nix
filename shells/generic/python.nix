22e1d32d2b418b236ff18b86f60490836e3fa041
Marc Neudert <marc.neudert@gmail.com>
2025-04-27 11:00:29 +0200

    [shells] Configure venv for python

--- shells/generic/python.nix
+++ shells/generic/python.nix
@@ -10,10 +10,11 @@ stdenv.mkDerivation rec {
   shellHook = ''
     export SHELL_DATA_DIR="/data/nix-shells/${name}"
 
+    virtualenv "$SHELL_DATA_DIR/venv"
+    source "$SHELL_DATA_DIR/venv/bin/activate"
+
     export PATH="$SHELL_DATA_DIR/bin:$PATH"
     export PS1="[generic:python|\[\e[1m\]\w\[\e[0m\]]$ "
-    export PYTHONUSERBASE="$SHELL_DATA_DIR"
-    export PYTHONPATH="$(python -m site --user-site):$PYTHONPATH"
   '';
 
   buildInputs = [
@@ -24,5 +25,6 @@ stdenv.mkDerivation rec {
     python3Packages.pycodestyle
     python3Packages.pytest
     python3Packages.setuptools
+    python3Packages.virtualenv
   ];
 }
