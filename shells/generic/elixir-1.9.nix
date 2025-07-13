87ea597208121654f6a447adb5cb4d6a971f2d90
Marc Neudert <marc.neudert@gmail.com>
2025-07-13 11:16:27 +0200

    [shells] Install Elixir 1.9 from nixos-22.05 channel

--- shells/generic/elixir-1.9.nix
+++ shells/generic/elixir-1.9.nix
@@ -1,11 +1,14 @@
-with import <nixos-22.11> { };
+with import <nixos-22.05> { };
 
 let
-  pkgs = import <nixos-22.11> {
+  pkgs = import <nixos-22.05> {
     config = {
       permittedInsecurePackages = [ "erlang-22.3.4.24" ];
     };
   };
+
+  erlang = pkgs.beam.interpreters.erlangR22;
+  elixir = pkgs.beam.packages.erlangR22.elixir_1_9;
 in
 stdenv.mkDerivation rec {
   name = "generic-shell-elixir-1.9";
@@ -23,13 +26,9 @@ stdenv.mkDerivation rec {
     export PS1="[generic:elixir-1.9|\[\e[1m\]\w\[\e[0m\]]$ "
   '';
 
-  elixir = pkgs.callPackage /data/projects/private/configuration.nix/packages/elixir-1.9 {
-    erlang = pkgs.erlangR22;
-  };
-
   buildInputs = with pkgs; [
     glibcLocales
     elixir
-    erlangR22
+    erlang
   ];
 }
