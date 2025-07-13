f66f6d55bea22da3eb697fde4bb31a61704a6222
Marc Neudert <marc.neudert@gmail.com>
2025-07-13 11:15:23 +0200

    [shells] Install Elixir 1.7 from nixos-21.11 channel

--- shells/generic/elixir-1.7.nix
+++ shells/generic/elixir-1.7.nix
@@ -1,11 +1,14 @@
-with import <nixos-22.11> { };
+with import <nixos-21.11> { };
 
 let
-  pkgs = import <nixos-22.11> {
+  pkgs = import <nixos-21.11> {
     config = {
       permittedInsecurePackages = [ "erlang-22.3.4.24" ];
     };
   };
+
+  erlang = pkgs.beam.interpreters.erlangR22;
+  elixir = pkgs.beam.packages.erlangR22.elixir_1_7;
 in
 stdenv.mkDerivation rec {
   name = "generic-shell-elixir-1.7";
@@ -23,13 +26,9 @@ stdenv.mkDerivation rec {
     export PS1="[generic:elixir-1.7|\[\e[1m\]\w\[\e[0m\]]$ "
   '';
 
-  elixir = pkgs.callPackage /data/projects/private/configuration.nix/packages/elixir-1.7 {
-    erlang = pkgs.erlangR22;
-  };
-
   buildInputs = with pkgs; [
     glibcLocales
     elixir
-    erlangR22
+    erlang
   ];
 }
