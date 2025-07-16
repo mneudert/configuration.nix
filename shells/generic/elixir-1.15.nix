19afe48a8e96c282cb035ee9bd500d34d322d9ff
Marc Neudert <marc.neudert@gmail.com>
2025-07-16 14:21:30 +0200

    [shells] Install Elixir 1.15 from nixos-24.11 channel

--- shells/generic/elixir-1.15.nix
+++ shells/generic/elixir-1.15.nix
@@ -1,5 +1,9 @@
 with import <nixos-24.11> { };
 
+let
+  erlang = pkgs.beam.interpreters.erlang_26;
+  elixir = pkgs.beam.packages.erlang_26.elixir_1_15;
+in
 stdenv.mkDerivation rec {
   name = "generic-shell-elixir-1.15";
   env = buildEnv {
@@ -16,13 +20,9 @@ stdenv.mkDerivation rec {
     export PS1="[generic:elixir-1.15|\[\e[1m\]\w\[\e[0m\]]$ "
   '';
 
-  elixir = pkgs.callPackage /data/projects/private/configuration.nix/packages/elixir-1.15 {
-    erlang = erlang_26;
-  };
-
   buildInputs = [
     glibcLocales
     elixir
-    erlang_26
+    erlang
   ];
 }
