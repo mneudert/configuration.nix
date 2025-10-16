e8327218fca6632ca39ca29cd74823198ae352b6
Marc Neudert <marc.neudert@gmail.com>
2025-10-16 14:43:09 +0200

    [packages] elixir-1.19: init at 1.19.0

---
+++ shells/generic/elixir-1.19.nix
@@ -0,0 +1,26 @@
+with import <nixpkgs> { };
+
+let
+  erlang = pkgs.beam.interpreters.erlang_28;
+  elixir = pkgs.callPackage /data/projects/private/configuration.nix/packages/elixir-1.19 {
+    erlang = erlang_28;
+  };
+in
+stdenv.mkDerivation rec {
+  name = "generic-shell-elixir-1.19";
+
+  shellHook = ''
+    export SHELL_DATA_DIR="/data/nix-shells/${name}"
+
+    export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$SHELL_DATA_DIR/erlang-history\"'"
+    export HEX_HOME="$SHELL_DATA_DIR/hex"
+    export MIX_HOME="$SHELL_DATA_DIR/mix"
+    export PS1="[generic:elixir-1.19|\[\e[1m\]\w\[\e[0m\]]$ "
+  '';
+
+  buildInputs = [
+    glibcLocales
+    elixir
+    erlang
+  ];
+}
