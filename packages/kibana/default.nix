58efc2b5e3b6eb4e6440acebb57af0b8a4e4eac5
Marc Neudert <marc.neudert@gmail.com>
2025-12-28 11:36:35 +0100

    [packages] kibana: 9.2.2 -> 9.2.3

--- packages/kibana/default.nix
+++ packages/kibana/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   name = "kibana-${version}";
-  version = "9.2.2";
+  version = "9.2.3";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/kibana/kibana-${version}-linux-x86_64.tar.gz";
-    hash = "sha256-kwTSqfFclmuZwP/5MuKTFijeq0tl+OdZk6Ef1E+oe4s=";
+    hash = "sha256-Bbixg7k5IPi2REu3II4PrkDxKlWJwLYA3+48H41QbtY=";
   };
 
   installPhase = ''
