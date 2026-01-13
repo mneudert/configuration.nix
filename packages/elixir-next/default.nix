49de3512da26e1b670b92e2c600337217f9e550a
Marc Neudert <marc.neudert@gmail.com>
2026-01-13 14:38:08 +0100

    [packages] elixir-next: 1.20.0-rc.0 -> 1.20.0-rc.1

--- packages/elixir-next/default.nix
+++ packages/elixir-next/default.nix
@@ -12,13 +12,13 @@
 
 stdenv.mkDerivation rec {
   name = "elixir-${version}";
-  version = "1.20.0-rc.0";
+  version = "1.20.0-rc.1";
 
   src = fetchFromGitHub {
     owner = "elixir-lang";
     repo = "elixir";
     rev = "v${version}";
-    hash = "sha256-pysf7rYV+VVImbA4F8TW1P2JVJN9pWgJwHfPGz4H5M8=";
+    hash = "sha256-FuTZHDI8ZNe6SHjiaPDZh21Ah7ek4kHqlYVvx0ybqI4=";
   };
 
   buildInputs = [
