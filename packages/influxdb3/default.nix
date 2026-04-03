e10e1622c3e2a2eac6da1a1f9f707433065ca50f
Marc Neudert <marc.neudert@gmail.com>
2026-04-03 09:30:33 +0200

    [packages] influxdb3: 3.8.3 -> 3.9.0

--- packages/influxdb3/default.nix
+++ packages/influxdb3/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   name = "influxdb3-${version}";
-  version = "3.8.3";
+  version = "3.9.0";
 
   src = fetchurl {
     url = "https://dl.influxdata.com/influxdb/releases/influxdb3-core-${version}_linux_amd64.tar.gz";
-    hash = "sha256-4VTZ0rsCUR1QTZ5CS/1vUe+Phx2snr1MJP5e1dW+YW4=";
+    hash = "sha256-Up/EDbphuv0djQt5C7ONipjBKyAFFEVHU6sPeDoagUg=";
   };
 
   dontPatchELF = true;
