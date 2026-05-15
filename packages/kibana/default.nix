9e948cdd0b59a8ec38057eeb717c574fd38624c5
Marc Neudert <marc.neudert@gmail.com>
2026-05-15 13:16:53 +0200

    [packages] kibana: 9.4.0 -> 9.4.1

--- packages/kibana/default.nix
+++ packages/kibana/default.nix
@@ -6,11 +6,11 @@
 
 stdenv.mkDerivation rec {
   name = "kibana-${version}";
-  version = "9.4.0";
+  version = "9.4.1";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/kibana/kibana-${version}-linux-x86_64.tar.gz";
-    hash = "sha256-gmohBoUUF40kKZqQZWMdofAYudPVOOYVbQdEYEvDErc=";
+    hash = "sha256-UhEEInOfqc2nJB4xTRlRkRBF+n95P8jo9vMPOhuxWIU=";
   };
 
   installPhase = ''
