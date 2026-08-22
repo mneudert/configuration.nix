419268aa6c7f7359d4e611e476d09485757c2299
Marc Neudert <marc.neudert@gmail.com>
2026-08-22 10:29:59 +0200

    [packages] elasticsearch: 9.5.1 -> 9.5.2

--- packages/elasticsearch/default.nix
+++ packages/elasticsearch/default.nix
@@ -7,11 +7,11 @@
 
 stdenv.mkDerivation rec {
   name = "elasticsearch-${version}";
-  version = "9.5.1";
+  version = "9.5.2";
 
   src = fetchurl {
     url = "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${version}-linux-x86_64.tar.gz";
-    hash = "sha256-jLt+tkF5Qw9LJnllw8wfMrEv8Oyj4PvwRFDd1nw7R4s=";
+    hash = "sha256-EcakSvb7OFOXDJbrFs8MaaOoRvu9e/scfOssMteONsQ=";
   };
 
   patches = [ ./elasticsearch-env.patch ];
